25748c220c688af3615c5115b98c4b44eadeb4b0
doc <sesygiallo@gmail.com>
2025-05-15 10:25:36 +0200

    starting to config nvim with NVF

--- flake.nix
+++ flake.nix
@@ -3,34 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     catppuccin.url = "github:catppuccin/nix";
-
     hyprland.url = "github:hyprwm/Hyprland";
+    nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # use "nixos", or your hostname as the name of the configuration
-    # it's a better practice than "default" shown in the video
+  outputs = { self, nixpkgs, nvf, ... }@inputs: {
+
+    packages."x86_64-linux".default =
+      (nvf.lib.neovimConfiguration {
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+	modules = [ ./nvf-configuration.nix ];
+      }).neovim;
+
     nixosConfigurations = {
       laptop = nixpkgs.lib.nixosSystem {
       	# system = "x86_64-linux";
         specialArgs = { inherit inputs; };
-	      modules = [
-	        ./hosts/laptop/configuration.nix
-	        ./modules/nvidia.nix
-	        ./modules/gnome.nix
-	      ];
+	  modules = [
+	    ./hosts/laptop/configuration.nix
+	    ./modules/nvidia.nix
+	    ./modules/gnome.nix
+	  ];
       };
     };
   };
---
+++ nvf-configuration.nix
@@ -0,0 +1,25 @@
+{ pkgs, lib, ... }:
+
+{
+  vim = {
+    theme = {
+      enable = true;
+      name = "catppuccin";
+      style = "frappuccin"
+    };
+
+    statusline.lualine.enable = true;
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+  };
+
+  languages = {
+    enableLSP = true;
+    enableTreesitter = true;
+
+    nix.enable;
+    ts.enable;
+    go.enable;
+    python.enable;
+  };
+}
