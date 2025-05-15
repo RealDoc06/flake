6f4d44663a8972404fe69c6b0a4b5de0eab87a5c
doc <sesygiallo@gmail.com>
2025-05-15 12:11:34 +0200

    fixed auto indentation

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -8,17 +8,29 @@
       style = "frappe";
     };
 
+    # Auto tab
+    globals.editorconfig = true;
+    utility.sleuth.enable = true;
+
+    lsp.enable = true;
+    
+#    options = {  
+#      shiftwidth = 2;
+#    };
+
     statusline.lualine.enable = true;
     telescope.enable = true;
     autocomplete.nvim-cmp.enable = true;
 
     languages = {
-      enableLSP = true;
       enableTreesitter = true;
 
       nix.enable = true;
+      lua.enable = true;
+      css.enable = true;
+      html.enable = true;
       ts.enable = true;
-      #go-lang.enable = true;
+      go.enable = true;
       python.enable = true;
     };
   };
